7e17d53915fad7a0d25df6bf31adf0b9fb242113
Samuel Costa <samukaka1000@gmail.com>
2025-08-12 18:08:02 -0400

    Back button to main menu implemented in main_screen

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -7,21 +7,14 @@ import 'dice_roll_screen.dart';
 import 'initiatives.dart';
 import 'config_screen.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: _MainScreen());
-  }
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreen extends StatefulWidget {
-  @override
-  State<_MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<_MainScreen> {
+class _MainScreenState extends State<MainScreen> {
   int _selectedItemIndex = 0;
 
   final List<Widget> _pages = [
@@ -42,6 +35,16 @@ class _MainScreenState extends State<_MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Games"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+
       body: IndexedStack(index: _selectedItemIndex, children: _pages),
 
       bottomNavigationBar: BottomNavigationBar(
